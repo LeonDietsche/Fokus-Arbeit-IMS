d28ba3e82d1e38d6364c6ce2d758f1a98fbefc90
Leon Dietsche <dietscheleon@gmail.com>
2020-06-19 09:18:42 +0200

    First commit

---
+++ includes/AbstractCronController.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+abstract class AbstractCronController {
+
+  private $dbServername;
+  private $dbUsername;
+  private $dbPassword;
+  private $dbName;
+
+
+protected function connect(){
+$this->dbServername = "localhost";
+$this->dbUsername = "root";
+$this->dbPassword = "";
+$this->dbName = "testdb"
+$conn = new sqli($this->dbServername, $this->dbUsername, $this->dbPassword, $this->dbName);
+return $conn;
+}
+
+abstract public function callCronJob();
+abstract public function performCronJob();
+
+}
+
+ ?>
